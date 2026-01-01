e20ae4d7bebc8bbd1787297d90f2a329f4193924
chanbasha <chanbahsa@protonmail.com>
2026-01-01 20:23:11 +0530

    new commit

--- src/jea/test1/ex1/Test1.java
+++ src/jea/test1/ex1/Test1.java
@@ -7,7 +7,7 @@ public class Test1 {
     public static void main(String[] args) {
         Integer[] numbers = {5, 2, 9, 1, 7};
         //before sort
-        Arrays.sort(numbers);
+      //  Arrays.sort(numbers);
 
         System.out.println("Original array: " + Arrays.toString(numbers));
 

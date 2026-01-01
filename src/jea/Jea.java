8518a0323dd6fa788f3c13b1add07aa6b52c9178
chanbasha <chanbahsa@protonmail.com>
2026-01-01 17:55:22 +0530

    test commit

--- src/jea/Jea.java
+++
@@ -1,7 +0,0 @@
-package jea;
-
-public class Jea {
-    public static void main(String[] args) {
-        System.out.println("Welcome to JavaEduAcademy");
-    }
-}
---
+++ src/jea/test1/ex1/Test1.java
@@ -0,0 +1,19 @@
+package jea.test1.ex1;
+
+import java.util.Arrays;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        Integer[] numbers = {5, 2, 9, 1, 7};
+        //before sort
+        Arrays.sort(numbers);
+
+        System.out.println("Original array: " + Arrays.toString(numbers));
+
+        // Sorting the array in ascending orders
+        Arrays.sort(numbers);
+
+        System.out.println("Sorted array (ascending): " + Arrays.toString(numbers));
+    }
+}
